f6ff14db40c58c3a897bc8c00f9080b8e5e57a66
kishorpandya2210 <kishorpandya17@gmail.com>
2022-01-15 04:16:31 -0500

    Changed final to const in secret file

--- lib/secret.dart
+++ lib/secret.dart
@@ -1,2 +1,2 @@
-final String secret = "14b56bf3374784c8920d16e68c51fcf7b79d2250";
-final String clientID = "76630";
+const String secret = "14b56bf3374784c8920d16e68c51fcf7b79d2250";
+const String clientID = "76630";
