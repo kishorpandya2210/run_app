d49c470adc15931a5f1268f541bcfdf5c1d9a863
Mit Patel <mitpatel9828@gmail.com>
2022-01-14 23:20:29 -0500

    redesign log in page

--- lib/login_strava.dart
+++ lib/login_strava.dart
@@ -9,15 +9,60 @@ class _StravaLoginState extends State<StravaLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('login with Strava'),
-      ),
-      body: Container(
-        child: RaisedButton(
-          child: Text('Connect with Strava'),
-          onPressed: () {},
-        ),
-      ),
-    );
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Image.asset('lib/Images/big running icon.png'),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Run App',
+                      style: TextStyle(
+                          color: Colors.cyan,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 45),
+                    )),
+                FlatButton(
+                  onPressed: (){
+                    //Strava login screen
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.teal,
+                          Colors.greenAccent
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                            offset: Offset(5, 5),
+                          blurRadius: 10,
+                        )
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Connect to Strava',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            
+              ],
+      )));
   }
 }
